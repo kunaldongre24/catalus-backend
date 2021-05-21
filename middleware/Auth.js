f4ce32f3d21b4705a09d771bcef5158c342f45a9
kunaldongre24 <dongrekunal16@gmail.com>
2021-05-21 12:21:31 +0530

    authcheckk

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 function Auth(req, res, next) {
   const token = req.header("accessToken");
   if (!token) {
-    return res.sendStatus(401).send("access denied");
+    return res.sendStatus(401);
   }
   try {
     const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = verified;
     next();
   } catch (err) {
-    res.sendStatus(400).send("Invalid Token");
+    res.send(err);
   }
 }
 
