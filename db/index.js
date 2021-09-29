03420b353c4aa5adbe5d72693f4a02b84de9b084
kunaldongre24 <dongrekunal16@gmail.com>
2021-09-29 15:44:16 +0530

    change the host of the database

--- db/index.js
+++ db/index.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 const { DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } =
   process.env;
 const db = mysql.createConnection({
-  host: sql6.freemysqlhosting.net,
-  user: sql6440988,
-  password: iS1K7zk5ju,
-  database: sql6440988,
+  host: "sql6.freemysqlhosting.net",
+  user: "sql6440988",
+  password: "iS1K7zk5ju",
+  database: "sql6440988",
 });
 const connection = db.connect((err) => {
   if (err) {
