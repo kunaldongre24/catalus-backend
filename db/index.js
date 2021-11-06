294b839fb5678a33af84acdc509d9ce4cefc71cd
kunaldongre24 <dongrekunal16@gmail.com>
2021-11-06 14:54:33 +0530

    kunal

--- db/index.js
+++ db/index.js
@@ -5,14 +5,30 @@ const { DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } =
 const db = mysql.createConnection({
   host: "remotemysql.com",
   user: "Xsb7TaLd7r",
-  password: "LAmDLAuJ8r",
+  password: "VqBgTVeQwh",
   database: "Xsb7TaLd7r",
 });
-const connection = db.connect((err) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log("connected to the database...");
-  }
-});
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db);
+
+  connection.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
 export default db;
