d8bdeb957aa7f13ae20137e8198074ef17119eac
kunaldongre24 <dongrekunal16@gmail.com>
2021-11-11 09:20:36 +0530

    changes in files

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 const authRoute = require("./Auth");
 const userRoute = require("./User");
-const staffRoute = require("./Staff");
 const fileRoute = require("./File");
-const batchRoute = require("./Batch");
 const express = require("express");
 const router = express.Router();
 
 router.use("/auth", authRoute);
 router.use("/upload", fileRoute);
 router.use("/user", userRoute);
-router.use("/staff", staffRoute);
-router.use("/batch", batchRoute);
-router.use("/subject", batchRoute);
 
 module.exports = router;
